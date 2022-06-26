1c1c1ad856eaba98bf2070b6b4f80a1815f5fb62
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-06-26 14:00:09 +0000

    Finished search by author

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -12,31 +12,56 @@ export class Content extends Component {
 
     this.state = {
       isLoaded: false,
-
+      posts: [],
     }
   }
 
-componentDidMount(){
-  console.log("mounted")
-  setTimeout(() => {
-    this.setState({
-      isLoaded: true,
-    })
-  }, 3000)
-}
+  componentDidMount() {
+    console.log("mounted")
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+        posts: savedPosts,
+      })
+    }, 1000)
+  }
 
+  handleChange = (e) => {
+    const name = e.target.value.toLowerCase();
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name);
+    })
+    console.log(filteredPosts)
+    this.setState({
+      posts: filteredPosts
+    })
+  }
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
           <h1>My Photos</h1>
+          <form>
+            <label htmlFor="searchInput"></label>
+            <input
+              type="search"
+              id="searchInput"
+              name="searchInput"
+              placeholder="By Author"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <h4>posts found {this.state.posts.length}</h4>
+          </form>
         </div>
-        {this.state.isLoaded ? <div className={css.SearchResults}>
-          <PostItem savedPosts={savedPosts} />
-        </div>
-        :
-        <Loader /> }
 
+        <div className={css.SearchResults}>
+          {
+            this.state.isLoaded ?
+              <PostItem savedPosts={this.state.posts} />
+              :
+              <Loader />
+          }
+        </div>
       </div>
     )
   }
